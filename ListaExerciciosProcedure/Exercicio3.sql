20713be212e12358c1a9953df75170a6e7afe503
Vitor Teixeira Silva <128430732+Vittutexx@users.noreply.github.com>
2024-04-17 14:29:09 -0300

    Corrigindo Exercicio3.sql

--- ListaExerciciosProcedure/Exercicio3.sql
+++ ListaExerciciosProcedure/Exercicio3.sql
@@ -1,9 +1,14 @@
----3. Crie uma função em PL/SQL chamada "CalcularValorDescontoPedido" que recebe o código de um
+---3. Crie uma funÃ§Ã£o em PL/SQL chamada "CalcularValorDescontoPedido" que recebe o cÃ³digo de um
 ---pedido como entrada e retorna o valor total do desconto aplicado no pedido, somando os descontos de todos os itens do pedido.
 CREATE OR REPLACE FUNCTION CalcularValorDescontoPedido(VCOD_PEDIDO PEDIDO.COD_PEDIDO%TYPE)RETURN NUMBER IS
-CODIGO_PEDIDO NUMBER;
-RAISE EXCP EXCEPTION;
+VALOR_TOTAL NUMBER;
+VALOR_DESCONTO NUMBER := 0;
 BEGIN
-    SELECT VAL_DESCONTO INTO CODIGO_PEDIDO FROM PEDIDO WHERE COD_PEDIDO = VCOD_PEDIDO;
-    RETURN CODIGO_PEDIDO;
+    FOR X IN (SELECT VALOR_DESCONTO FROM PEDIDO WHERE COD_PEDIDO = VCOD_PEDIDO) LOOP
+        VALOR_DESCONTO := VALOR_DESCONTO + X.VALOR_DESCONTO;
+        
+    END LOOP;
+    VALOR_TOTAL := VALOR_DESCONTO;
+    
+    RETURN 'Valor Total do Desconto no pedido '|| VCOD_PEDIDO || ': ' || VALOR_TOTAL;
 END CalcularValorDescontoPedido;
