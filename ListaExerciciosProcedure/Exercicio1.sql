296115cd61dc64317b01d5ef88876240d702bc28
Vitor Teixeira Silva <128430732+Vittutexx@users.noreply.github.com>
2024-04-16 22:38:04 -0300

    Update Exercicio1.sql

--- ListaExerciciosProcedure/Exercicio1.sql
+++ ListaExerciciosProcedure/Exercicio1.sql
@@ -1,18 +1,18 @@
 SET SERVEROUTPUT ON
----1. Escreva uma funÁ„o em PL/SQL chamada
----"CalcularTotalPedido" que recebe o cÛdigo de um pedido como par‚metro e retorna o valor total do pedido, calculado somando
+---1. Escreva uma fun√ß√£o em PL/SQL chamada
+---"CalcularTotalPedido" que recebe o c√≥digo de um pedido como par√¢metro e retorna o valor total do pedido, calculado somando
 ---os valores dos itens do pedido.
 CREATE OR REPLACE FUNCTION CalcularTotalPedido(VCOD_PEDIDO PEDIDO.COD_PEDIDO%TYPE
 ) RETURN NUMBER IS
 VALOR_TOTAL NUMBER;
 BEGIN
     
-        VALOR_TOTAL := 0;
-            FOR X IN(SELECT * FROM PEDIDO WHERE COD_PEDIDO = VCOD_PEDIDO) LOOP
-                DBMS_OUTPUT.PUT_LINE('X.VAL_TOTAL_PEDIDO');
-                VALOR_TOTAL := VALOR_TOTAL + X.VAL_TOTAL_PEDIDO;
-            END LOOP;        
-           DBMS_OUTPUT.PUT_LINE('Valor total do Pedido: ' || VCOD_PEDIDO || CHR(10) || ' ' || VALOR_TOTAL);
-
+    VALOR_TOTAL := 0;
+    FOR X IN(SELECT * FROM PEDIDO WHERE COD_PEDIDO = VCOD_PEDIDO) LOOP
+        DBMS_OUTPUT.PUT_LINE('X.VAL_TOTAL_PEDIDO');
+        VALOR_TOTAL := VALOR_TOTAL + X.VAL_TOTAL_PEDIDO;
+    END LOOP;        
+    DBMS_OUTPUT.PUT_LINE('Valor total do Pedido: ' || VCOD_PEDIDO || CHR(10) || ' ' || VALOR_TOTAL);
+    
     RETURN VALOR_TOTAL;
 END CalcularTotalPedido;    
