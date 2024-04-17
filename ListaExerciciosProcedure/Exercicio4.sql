a5fdb896bda57d969130e4d98b1924c0b8c0b02a
Vitor Teixeira Silva <128430732+Vittutexx@users.noreply.github.com>
2024-04-17 15:18:35 -0300

    Corrigindo Exercicio4.sql

--- ListaExerciciosProcedure/Exercicio4.sql
+++ ListaExerciciosProcedure/Exercicio4.sql
@@ -1,5 +1,5 @@
----4. Escreva um procedimento em PL/SQL denominado "ListarPedidosCliente" que recebe o código de um cliente como parâmetro e lista todos os pedidos associados a 
----esse cliente, incluindo detalhes como o número do pedido, a data do pedido e o valor total.
+---4. Escreva um procedimento em PL/SQL denominado "ListarPedidosCliente" que recebe o cÃ³digo de um cliente como parÃ¢metro e lista todos os pedidos associados a 
+---esse cliente, incluindo detalhes como o nÃºmero do pedido, a data do pedido e o valor total.
 SET SERVEROUTPUT ON
 CREATE OR REPLACE PROCEDURE ListarPedidosDoCliente(VAL_COD_CLIENTE PEDIDO.COD_CLIENTE%TYPE) IS 
 EXCP EXCEPTION;
@@ -9,18 +9,10 @@ BEGIN
         END IF;
         FOR X IN (SELECT * FROM PEDIDO WHERE COD_CLIENTE = VAL_COD_CLIENTE) LOOP
         DBMS_OUTPUT.PUT_LINE(' CODIGO DO PEDIDO: ' || X.COD_PEDIDO || CHR(10) ||
-                             ' CODIGO DO PEDIDO RELACIONADO: ' || X.COD_PEDIDO_RELACIONADO || CHR(10) ||
-                             ' CODIGO DO CLIENTE: ' || X.COD_CLIENTE || CHR(10) ||
-                             ' CODIGO DO USUARIO: ' || X.COD_USUARIO || CHR(10) ||
-                             ' CODIGO DO VENDEDOR: ' || X.COD_VENDEDOR || CHR(10) ||
                              ' DATA DO PEDIDO: ' || X.DAT_PEDIDO || CHR(10) ||
-                             ' DATA DE CANCELAMENTO: ' || X.DAT_CANCELAMENTO || CHR(10) ||
-                             ' DATA DE ENTREGA DO PEDIDO: ' || X.DAT_ENTREGA || CHR(10) ||
-                             ' VALOR TOTAL DO PEDIDO: ' || X.VAL_TOTAL_PEDIDO || CHR(10) ||
-                             ' VALOR DO DESCONTO: ' || X.VAL_DESCONTO || CHR(10) ||
-                             ' SEQUENCIA DO ENDEREÇO DO CLIENTE: ' || X.SEQ_ENDERECO_CLIENTE || CHR(10));
+                             ' VALOR TOTAL DO PEDIDO: ' || X.VAL_TOTAL_PEDIDO || CHR(10));
         END LOOP;
         EXCEPTION
             WHEN EXCP THEN
-                RAISE_APPLICATION_ERROR(-20020, 'O limite de números para o código deve ser de até 10 e não deve conter caracteres');
+                RAISE_APPLICATION_ERROR(-20020, 'O limite de nÃºmeros para o cÃ³digo deve ser de atÃ© 10 e nÃ£o deve conter caracteres');
 END;
